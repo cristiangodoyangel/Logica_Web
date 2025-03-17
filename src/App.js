f80ddb51415462f59c33d725e73383c8b5cb7c95
cristiangodoyangel <creacciones@gmail.com>
2025-03-17 14:27:41 -0300

    Agregado navbar fijo en todas las páginas

--- src/App.js
+++ src/App.js
@@ -13,92 +13,32 @@ function App() {
   return (
     <Router>
       <div className="min-vh-100 d-flex flex-column bg-dark-logica">
-        <Routes>
-          <Route path="/" element={
-            <>
-              {/* Navbar */}
-              <Navbar variant="dark" expand="lg" className="navbar-custom">
-                <Container>
-                  <Navbar.Brand as={Link} to="/">
-                    <img src="/logica.png" height="40" className="me-2" alt="Logica Logo" />
-                  </Navbar.Brand>
-                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                      <Nav.Link as={Link} to="/servicios">Servicios</Nav.Link>
-                      <Nav.Link as={Link} to="/nosotros">Nosotros</Nav.Link>
-                      <Nav.Link as={Link} to="/contacto">Contacto</Nav.Link>
-                    </Nav>
-                  </Navbar.Collapse>
-                </Container>
-              </Navbar>
-              <Home />
-            </>
-          } />
-          <Route path="/servicios" element={
-            <>
-              {/* Navbar */}
-              <Navbar variant="dark" expand="lg" className="navbar-custom">
-                <Container>
-                  <Navbar.Brand as={Link} to="/">
-                    <img src="/logica.png" height="40" className="me-2" alt="Logica Logo" />
-                  </Navbar.Brand>
-                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                      <Nav.Link as={Link} to="/servicios">Servicios</Nav.Link>
-                      <Nav.Link as={Link} to="/nosotros">Nosotros</Nav.Link>
-                      <Nav.Link as={Link} to="/contacto">Contacto</Nav.Link>
-                    </Nav>
-                  </Navbar.Collapse>
-                </Container>
-              </Navbar>
-              <Servicios />
-            </>
-          } />
-          <Route path="/nosotros" element={
-            <>
-              {/* Navbar */}
-              <Navbar variant="dark" expand="lg" className="navbar-custom">
-                <Container>
-                  <Navbar.Brand as={Link} to="/">
-                    <img src="/logica.png" height="40" className="me-2" alt="Logica Logo" />
-                  </Navbar.Brand>
-                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                      <Nav.Link as={Link} to="/servicios">Servicios</Nav.Link>
-                      <Nav.Link as={Link} to="/nosotros">Nosotros</Nav.Link>
-                      <Nav.Link as={Link} to="/contacto">Contacto</Nav.Link>
-                    </Nav>
-                  </Navbar.Collapse>
-                </Container>
-              </Navbar>
-              <Nosotros />
-            </>
-          } />
-          <Route path="/contacto" element={
-            <>
-              {/* Navbar */}
-              <Navbar variant="dark" expand="lg" className="navbar-custom">
-                <Container>
-                  <Navbar.Brand as={Link} to="/">
-                    <img src="/logica.png" height="40" className="me-2" alt="Logica Logo" />
-                  </Navbar.Brand>
-                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                      <Nav.Link as={Link} to="/servicios">Servicios</Nav.Link>
-                      <Nav.Link as={Link} to="/nosotros">Nosotros</Nav.Link>
-                      <Nav.Link as={Link} to="/contacto">Contacto</Nav.Link>
-                    </Nav>
-                  </Navbar.Collapse>
-                </Container>
-              </Navbar>
-              <Contacto />
-            </>
-          } />
-        </Routes>
+        {/* Navbar - Ahora fuera de las rutas y con fixed="top" */}
+        <Navbar variant="dark" expand="lg" className="navbar-custom" fixed="top">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              <img src="/logica.png" height="40" className="me-2" alt="Logica Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link as={Link} to="/servicios">Servicios</Nav.Link>
+                <Nav.Link as={Link} to="/nosotros">Nosotros</Nav.Link>
+                <Nav.Link as={Link} to="/contacto">Contacto</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        
+        {/* Contenedor principal con padding-top para compensar el navbar fijo */}
+        <div className="main-content pt-5">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/servicios" element={<Servicios />} />
+            <Route path="/nosotros" element={<Nosotros />} />
+            <Route path="/contacto" element={<Contacto />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
